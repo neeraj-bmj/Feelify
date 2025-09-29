5aeb2be7248d2241ec189082bf32ee864de04823
neeraj-bmj <neerajguptaica@gmail.com>
2025-09-29 16:40:57 +0530

    multer setup for file upload

--- Backend/src/routes/song.route.js
+++ Backend/src/routes/song.route.js
@@ -1,13 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const multer = require("multer");
 
+const upload = multer({ Storage: multer.memoryStorage() });
 
+router.post("/songs", upload.single("audio") ,(req, res) => {
+  // write your logic
+  console.log(req.body);
+  console.log(req.file); 
+  res.status(201).json({
+    message: "Song uploaded successful",
+    song : req.file,
+  });
+});
 
-
-router.get("/songs", (req, res)=>{
-    // write your logic
-    res.send("hello this is from songs")
-    
-})
+router.get("/songs", (req, res) => {
+  // write your logic
+  res.send("hello this is from songs");
+});
 
 module.exports = router;
